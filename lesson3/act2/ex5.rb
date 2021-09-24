6be7dda82ed71d70582f95014d67c534d22486d3
anastasiyazl <89596283+anastasiyazl@users.noreply.github.com>
2021-09-24 11:55:44 +0300

    new year

--- lesson3/act2/ex5.rb
+++ lesson3/act2/ex5.rb
@@ -1,7 +1,7 @@
 puts "Please enter any year"
 year = Integer gets
-remainderOfDivision = year % 400
-if (remainderOfDivision == 0)
+year % 400
+if ((year % 4 == 0) & (year % 100 != 0) || (year % 400 == 0))
     puts "leap year"
 else
     puts "common year"
